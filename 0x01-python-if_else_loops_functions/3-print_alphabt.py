01f55232d75a1492203de957759fce868710e81a
qwab17 <quadriwasiubiodun@gmail.com>
2023-10-31 17:39:12 -0700

    program that prints the ASCII alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,5 @@
 
 for letter in range(ord('a'), ord('z') + 1):
     if chr(letter) not in 'qe':
-        print("{:c}".format(letter), end='' if letter < ord('z') and chr(letter + 1) not in 'qe' else '')
+        print("{:c}".format(letter), end='' if letter < ord('z')
+                and chr(letter + 1) not in 'qe' else '')
