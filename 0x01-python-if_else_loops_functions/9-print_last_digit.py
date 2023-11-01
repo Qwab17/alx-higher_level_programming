de5d2dd50ac2d1a88c9a3d26c0c4947a4d69fc7f
qwab17 <quadriwasiubiodun@gmail.com>
2023-11-01 01:54:28 -0700

    fxn that prints last digits

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    """
+    Print the last digit of a number.
+
+    Args:
+        number (int): The input number.
+
+    Returns:
+        int: The last digit of the number.
+    """
+    last_digit = abs(number) % 10
+    print(last_digit, end='')
+    return last_digit
