948f7c0c8d3650d3727f5133b28d3a09bab15323
qwab17 <quadriwasiubiodun@gmail.com>
2023-11-01 02:00:58 -0700

    fxn that adds two integer

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Add two integers and return the result.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The sum of a and b.
+    """
+    return a + b
