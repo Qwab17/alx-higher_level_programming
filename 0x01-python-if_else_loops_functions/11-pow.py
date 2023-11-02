c2ab543444d2a9263ec53fdbd4f312ca9ae2c578
qwab17 <quadriwasiubiodun@gmail.com>
2023-11-01 23:11:10 -0700

    fxn that computes a to power b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
 
+
 def pow(a, b):
-    
-    """
-    Compute a to the power of b and return the value.
-
-    Args:
-        a (int): The base.
-        b (int): The exponent.
-
-    Returns:
-        int or float: The result of a ^ b.
-    """
-    result = 1
-
-    if b < 0:
-        a, b = 1 / a, -b
-
-    for _ in range(b):
-        result *= a
-
-    return result
+    """Return a to power of b."""
+    return (a ** b)
