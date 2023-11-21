5e5c74b0a896804d84d41d86429a28c2e33336c3
qwab17 <quadriwasiubiodun@gmail.com>
2023-11-21 04:34:50 -0800

    fxn that prints the first x elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+
+    size = 0
+    for length in my_list:
+        size = size + 1
+
+    ReturnValue = 0
     try:
-        for i in range(x):
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-    except (IndexError, TypeError):
-        pass
-    finally:
+        for y in range(0, x):
+            if type(my_list[y]) is int:
+                print("{:d}".format(my_list[y]), end="")
+                ReturnValue = ReturnValue + 1
+            else:
+                continue
+
         print()
-        return count
+        return ReturnValue
+
+    except IndexError:
+        pass
