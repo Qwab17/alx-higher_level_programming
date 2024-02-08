49c21ce6c55a81b5370b4c11fb2318ab4c819137
qwab17 <quadriwasiubiodun@gmail.com>
2024-02-08 05:41:38 -0800

    11 square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+
 """
 more class base
 """
 
-
 Rectangle = __import__('9-rectangle').Rectangle
-
 
 """
 Square class
 """
-
 
 class Square(Rectangle):
     """ Square Class """
